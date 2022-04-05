23a013e4d239f757f771dd3d928e745386060465
jLangenbahn <joelangenbahn@yahoo.com>
2022-04-05 16:31:45 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -2,6 +2,30 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [tasks, setTasks] = useState([
+    {
+        id: 1,
+        text: 'Doctors Appointment',
+        day: 'Feb 5th at 2:30pm',
+        reminder: true,
+    },
+    {
+        id: 2,
+        text: 'Dinner',
+        day: 'Feb 12th at 3:45pm',
+        reminder: true,
+    },
+    {
+        id: 3,
+        text: 'Insurance Interview',
+        day: 'Jan 2th at 1:30pm',
+        reminder: true,
+    },]
+  )
+  const name = "Joe"
+  const x = false
+
+
   return (
     <div className="App">
       <header className="App-header">
